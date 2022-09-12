28436a96d6dc0ce170f8beea3a6ca6354183aa44
Dssaenz <darwinsaenz105@gmail.com>
2022-09-11 23:12:21 -0500

    added 'footer' and 'add user' section

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,5 +2,7 @@ import CardCelebrity from './CardCelebrity/CardCelebrity';
 import WrapperList from './WrapperList/WrapperList';
 import SelectField from './SelectField/SelectField';
 import Navbar from './Navbar/Nabvar';
+import Footer from './Footer/Footer';
+import AddSection from './AddSection/AddSection';
 
-export { CardCelebrity, WrapperList, SelectField, Navbar };
+export { CardCelebrity, WrapperList, SelectField, Navbar, AddSection, Footer };
