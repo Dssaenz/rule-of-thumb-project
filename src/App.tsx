15150b26bd582c28b3981084f9986b3e15ff1cd3
Dssaenz <darwinsaenz105@gmail.com>
2022-09-11 21:32:42 -0500

    modified navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
 
 import './App.css';
-import Search from './assets/img/search.svg';
 import useSendVote from './hooks/useSendVote';
 import logo from './assets/img/pope-francis.png';
 import peopleImg from './assets/img/bg-people.png';
 import LikeIcon from './assets/img/thumbs-up.svg';
 import DislikeIcon from './assets/img/thumbs-down.svg';
-import { WrapperList, CardCelebrity, SelectField } from './components';
+import { WrapperList, CardCelebrity, SelectField, Navbar } from './components';
 
 function App() {
   const [listSection, setListSection] = useState<boolean>(true);
@@ -16,43 +15,7 @@ function App() {
 
   return (
     <>
-      <nav className='nav' role='navigation'>
-        <div className='max-centered'>
-          <h1 className='nav__logo'>Rule of thumb.</h1>
-          <button className='nav__hamburger icon-button'>
-            <svg width='25' height='20' xmlns='http://www.w3.org/2000/svg'>
-              <path
-                d='M0 0h25v4H0V0zm0 8h25v4H0V8zm0 8h25v4H0v-4z'
-                fill='#FFF'
-                fillRule='nonzero'
-              />
-            </svg>
-          </button>
-          <ul className='nav__links'>
-            <li>
-              <a href='#'>Past Trials</a>
-            </li>
-            <li>
-              <a href='#'>How It Works</a>
-            </li>
-            <li>
-              <a href='#'>Login / Sign Up</a>
-            </li>
-            <li>
-              <form>
-                <input
-                  className='nav__search-input'
-                  aria-label='search'
-                  type='text'
-                />
-                <button className='nav__search icon-button' type='submit'>
-                  <img src={Search} alt='search' />
-                </button>
-              </form>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <Navbar />
       <header className='hero'>
         <img
           className='hero__background'
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,6 @@
 import CardCelebrity from './CardCelebrity/CardCelebrity';
 import WrapperList from './WrapperList/WrapperList';
 import SelectField from './SelectField/SelectField';
+import Navbar from './Navbar/Nabvar';
 
-export { CardCelebrity, WrapperList, SelectField };
+export { CardCelebrity, WrapperList, SelectField, Navbar };
